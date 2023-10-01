dc776a54c4c6f572e1887e462fb43e2b198201bc
flavio <flavioborinjunior@gmail.com>
2023-10-01 09:47:24 -0300

    Consertado forma de input e exec

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # reversible_turing_machine
+
+```
+stack exec revturing-exe <filename.txt>
+stack exec revturing-exe entrada-quintupla.txt
+stack exec revturing-exe bb4.txt
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,15 @@
 module Main (main) where
 
 import Turing.Machine.Conversor
+import Turing.Machine.ClassicMachine (ClassicMachine)
 import Turing.Machine.Machine (TuringMachine (showDefinition, tmShowRun, showStats))
 import Turing.Reader (readTm)
+import System.Environment
 
-main :: IO ()
-main = do
-  classicTm <- readTm "definitions/entrada-quintupla.txt"
+runAll :: ClassicMachine -> IO ()
+runAll classicTm = do
   let (logC, cMachine) = tmShowRun classicTm
-  let (log1, revMachine) = tmShowRun $ toReversible classicTm
+      (log1, revMachine) = tmShowRun $ toReversible classicTm
 
   writeFile "saida.txt" $
     "Definição da máquina clássica:\n"
@@ -18,4 +19,14 @@ main = do
     "Definição da máquina reversível:\n"
       ++ showDefinition revMachine ++ "\n"
       ++ log1 ++ "\n"
-      ++ showStats revMachine ++ "\n\n" 
+      ++ showStats revMachine ++ "\n\n"
+      
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [fileName] -> do
+      classicTm <- readTm fileName
+      runAll classicTm
+    _ -> putStrLn "Use: stack exec Program arq" 
